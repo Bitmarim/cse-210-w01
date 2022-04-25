9ba5b29af746580e433bdaf85c79ae3d49170141
Bitmarim <will.p.harper@gmail.com>
2022-04-25 09:23:15 -0600

    Fixing git stuff, needed new commit.

--- helloworld.cs
+++ helloworld.cs
@@ -2,11 +2,13 @@ using System;
 
 namespace MyApp // NameSpace depends on Project Name
 {
-    internal class Program
+    internal class Program // bingus
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Bingus");
+
         }
     }
 }
