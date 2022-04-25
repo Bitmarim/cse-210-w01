1f667e10cf81d27cc5909d7e0e2cdd90a4963043
RandomSomething98 <will.p.harper@gmail.com>
2022-04-25 09:06:33 -0600

    Hello World project done

---
+++ helloworld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyApp // NameSpace depends on Project Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
